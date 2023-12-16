d685986786bf01a0dd8c3f02bcb53d6e550847cd
kobi krasnoff <krasnoff.kobi@gmail.com>
2023-12-16 09:03:11 +0200

    create title

--- pages/sample-page/SamplePage.tsx
+++ pages/sample-page/SamplePage.tsx
@@ -5,14 +5,55 @@ let props: any;
 
 function SamplePage(props: any): JSX.Element {
     const [propsStr, setPropsStr] = useState<string>();
+    const [titleStr, setTitleStr] = useState<string>();
+
+    const setTitle = (bankService: string) => {
+      switch(bankService) {
+        case 'balance account':
+          setTitleStr('תנועות אחרונות');
+          break;
+        case 'deposit cheque':
+          setTitleStr('הפקדות שיק');
+          break;
+        case 'money transfer':
+          setTitleStr('העברה מחשבון לחשבון');
+          break;
+        case 'exchange rate conversion':
+          setTitleStr('שער חליפין');
+          break;
+        case 'portfolio':
+          setTitleStr('תיק השקעות');
+          break;
+        case 'loan':
+          setTitleStr('הלוואה');
+          break;
+        case 'Ordering check books':
+          setTitleStr('הזמנת פנקסי שיקים');
+          break;
+        case 'order credit card':
+          setTitleStr('הזמנת כרטיסי אשראי');
+          break;
+        case 'contact banker':
+          setTitleStr('צור קשר עם הבנקאי');
+          break;
+        default:
+          setTitleStr('אחר');
+          break;
+      }
+    }
 
     useEffect(() => {
         setPropsStr(JSON.stringify(props));
+        setTitle(props.route.params.bankService);
     }, []);
 
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Sample Page</Text>
+        <Text style={{
+          fontFamily: 'Heebo-Bold', 
+          fontSize: 20
+        }}>{titleStr}</Text>
+        <Text>עמוד: </Text>
         <Text>{propsStr}</Text>
       </View>
     );
